64b96846f77ac8188e203992bf1d86e93d3c1986
fmhmillet <fernando.hernandez1@upr.edu>
2016-10-12 23:00:03 -0400

    Update Main.java
    Added Constructor for Reset Button. Having trouble pulling your commits, so I Couldn't add all of your changes.

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -1,6 +1,8 @@
+//import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Main {
+
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Minesweeper");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -12,7 +14,14 @@ public class Main {
 
 		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
 		myFrame.addMouseListener(myMouseAdapter);
-
+		   
+//		JButton button = new JButton();
+//	        myFrame.add(button);
+//		button.setLocation(200, 300);
+//		button.setSize(50,50);
+				
 		myFrame.setVisible(true);
+		
+		
 	}
 }
